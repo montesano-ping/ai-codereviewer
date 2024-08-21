8efbb4618f1841126f90c02f29bf45919eefccbb
Cameron Montesano <cameronmontesano@pingidentity.com>
2024-08-21 15:53:46 -0600

    make it mean 2

--- src/main.ts
+++ src/main.ts
@@ -86,8 +86,7 @@ function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
-- Make sure to be as insulting as possible.
-- IMPORTANT: Add "ayyy" to the end of each comment.
+- IMPORTANT:Make sure to be as insulting as possible.
 
 Review the following code diff in the file "${
     file.to
